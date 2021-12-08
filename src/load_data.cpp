cb7dd71e6af52e48996b9b83fe53cd6677a107a0
csmaster23 <tanneranorton23@gmail.com>
2021-12-07 19:21:27 -0700

    can write file for ceres solver in same format as ladybug

--- src/load_data.cpp
+++ src/load_data.cpp
@@ -254,6 +254,7 @@ void Dataloader::create_dataset()
     using Match = std::vector<int>;
     std::map<int, std::vector<Match>> match_map;
 
+    int obs_counter = 0;
     int point_counter = 0;
     for (int i=0; i < all_good_matches.size(); i++)
     {
@@ -265,7 +266,7 @@ void Dataloader::create_dataset()
             if(match_map.count(i) == 0)
             {
                 match_map[i] = std::vector<Match>();
-                //std::cout << "Creating empty vector" << std::endl;
+                std::cout << "Creating empty vector" << std::endl;
             }
             // For zeroeth camera, we add all matched feature points
             // between cameras 0 and 1 and increment point counter with
@@ -275,6 +276,7 @@ void Dataloader::create_dataset()
                 int curr_kf_idx = curr_match[j].trainIdx;
                 int next_kf_idx = curr_match[j].queryIdx;
                 match_map[i].push_back(Match({curr_kf_idx, next_kf_idx, point_counter}));
+                ++obs_counter;
                 ++point_counter;
             }
             // Pretend like we iterated to camera 2
@@ -285,7 +287,7 @@ void Dataloader::create_dataset()
         if(match_map.count(i) == 0)
         {
             match_map[i] = std::vector<Match>();
-            //std::cout << "Creating empty vector" << std::endl;
+            std::cout << "Creating empty vector" << std::endl;
         }
         int last_key = i-1;
         auto last_vector = match_map[last_key];
@@ -295,28 +297,35 @@ void Dataloader::create_dataset()
         {
             int curr_kf_idx = curr_match[j].trainIdx;
             int next_kf_idx = curr_match[j].queryIdx;
+
+            int point_number = -1;
             for(int k = 0; k < last_vector.size(); ++k)
             {
                 // Set to correctly consolidate duplicate points
                 // between camera matches
-                int point_number = -1;
+                // int point_number = -1;
                 int parent_query_kf = last_vector[k][1];
                 // Check if current feature match has already
                 // been found
                 if(parent_query_kf == curr_kf_idx)
-                {
+                { 
                     point_number = last_vector[k][2];
+                    std::cout << "parent == cur " << std::to_string(curr_kf_idx) << " " << std::to_string(next_kf_idx) << " " << std::to_string(point_number) << std::endl;
                     match_map[i].push_back(Match({
                         curr_kf_idx, next_kf_idx, point_number}));
+                    ++obs_counter;
+                    break; // *** I THINK THIS IS OK BUT NOT SURE! IT WOULD MEAN THAT VECTOR OF MATCH OBJECTS HAS UNIQUE KEY FEATURES
                 }
                 // Check if there is a new point according
                 // to our given feature indices
-                if(point_number == -1)
-                {
-                    match_map[i].push_back(Match({
-                        curr_kf_idx, next_kf_idx, point_counter}));
-                    ++point_counter;
-                }
+            }
+            if(point_number == -1)
+            {
+                std::cout << "Point counter in point_number == -1: " << std::to_string(point_counter) << std::endl;
+                match_map[i].push_back(Match({
+                    curr_kf_idx, next_kf_idx, point_counter}));
+                ++obs_counter;
+                ++point_counter;
             }
         }
         // Iterate through the last vector
@@ -324,8 +333,109 @@ void Dataloader::create_dataset()
     std::cout << "Match map size: " << match_map.size() << std::endl;
     std::cout << "Camera pose size: " << camera_poses.size() << std::endl;
 
-    // std::ifstream fin("./dataset/feature_points.txt");
 
+    // OPEN FILE TO WRITE TO
+    std::ofstream fout("../datasets/out_feature_points.txt");
+    fout << camera_poses.size() << " " << point_counter << " " << obs_counter << "\n";
+
+
+    std::vector<std::vector<double>> XYZ;
+    // Iterate over all map keys and values
+    // std::map<int, std::vector<Match>> match_map;
+    // Match = std::vector<int>;
+    for(std::map<int,std::vector<Match>>::iterator iter = match_map.begin(); iter != match_map.end(); ++iter)
+    {
+        int img_idx =  iter->first; // int
+        std::vector<Match> match_point_vec = iter->second; // vector of vectors of ints which are curr_kf_idx, next_kf_idx, point_number
+        std::cout << "\nCurrent Image index: " << std::to_string(img_idx) << std::endl;
+        std::cout << "All x, y, p in curr image: " << std::endl;
+        for (int jk=0; jk<match_point_vec.size(); jk++)
+        {
+            int key_feature_idx_1 = match_point_vec[jk][0];
+            // int key_feature_idx_2 = match_point_vec[jk][1];
+
+            std::cout << "\nkey point idx 1: " <<  key_feature_idx_1 << std::endl;
+            // std::cout << "key point idx 2: " <<  key_feature_idx_2 << std::endl;
+
+            int associated_point = match_point_vec[jk][2];
+            // std::cout << key_feature_idx_1 << " " << key_feature_idx_2 << " " << associated_point << std::endl;
+
+            std::vector<cv::KeyPoint> feature_vec_left = feature_vec[img_idx];
+            std::vector<double> depth_vec = feature_depth[img_idx];
+
+            std::cout << "feature_vec_left size: " <<  feature_vec_left.size() << std::endl;
+            std::cout << "depth_vec size: " <<  depth_vec.size() << std::endl;
+
+            cv::KeyPoint key_point = feature_vec_left[key_feature_idx_1];
+
+            double k1_x = key_point.pt.x;
+            double k1_y = key_point.pt.y;
+            int depth = depth_vec[key_feature_idx_1];
+
+            // Write to file, observations:
+            fout << img_idx << " " << associated_point << " " << k1_x << " " << k1_y << "\n";
+            
+            std::cout << "Key point one x: " << k1_x << " Key point one y: " << k1_y << std::endl;
+            std::cout << "Key point depth: " << depth << std::endl;
+            
+            double factor = 5000;
+            double fx = 525.0; 
+            double fy = 525.0;
+            double cx = 319.5;
+            double cy = 239.5;
+            
+            // in terms of the camera frame
+            double Z = depth / factor;
+            double X = (k1_x - cx) * Z / fx;
+            double Y = (k1_y - cy) * Z / fy;
+            std::cout << "X: " << std::to_string(X) << " Y: " << std::to_string(Y) << " Z: " << std::to_string(Z) << std::endl;
+
+            // store XYZ in camera frame
+            std::vector<double> temp_holder_vector;
+            temp_holder_vector.push_back(X);
+            temp_holder_vector.push_back(Y);
+            temp_holder_vector.push_back(Z);
+            XYZ.push_back(temp_holder_vector);
+        }
+    }
+
+    // write camera poses to file
+    // cam params
+    double fx = 525.0;
+    double fy = 525.0;
+    double cx = 319.5;
+    double cy = 239.5;
+    double d0 = 0.2624;
+    double d1 = -0.9531;
+    double d2 = -0.0054;
+    double d3 = 0.0026;
+    double d4 = 1.1633;
+    for (int jk=0; jk<camera_poses.size(); jk++)
+    {
+        //pose vector is of form tx ty tz qx qy qz qw
+        std::vector<double> pose_vector = camera_poses[jk];
+        for (int kl=0; kl<pose_vector.size(); kl++)
+        {
+            // tx ty tz qx qy qz qw fx fy cx cy d0 d1 d2 d3 d4
+            fout << pose_vector[kl] << "\n";
+        }
+        fout << fx << "\n";
+        fout << fy << "\n";
+        fout << cx << "\n";
+        fout << cy << "\n";
+        fout << d0 << "\n";
+        fout << d1 << "\n";
+        fout << d2 << "\n";
+        fout << d3 << "\n";
+        fout << d4 << "\n";
+
+    }
+
+    for (int xyz=0; xyz<XYZ.size(); xyz++)
+    {
+        fout << XYZ[xyz][0] << "\n" <<  XYZ[xyz][1] << "\n" << XYZ[xyz][2] << "\n"; 
+    }
+    fout.close();
 
 }
 
