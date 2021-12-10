eac7c56c40491e0fb1c6f82e561da82869756578
Taylor Pool <taylorpool.27@gmail.com>
2021-12-09 18:03:07 -0700

    Added support for TUM dataset

--- include/bundle_adjustment.hpp
+++ include/bundle_adjustment.hpp
@@ -93,4 +93,82 @@ struct ReprojectionError
         double observed_x_;
         double observed_y_;
 };
+
+enum TumCamera
+{
+    TranslateX,
+    TranslateY,
+    TranslateZ,
+    QuaternionX,
+    QuaternionY,
+    QuaternionZ,
+    QuaternionW,
+    FocalX,
+    FocalY,
+    CenterX,
+    CenterY,
+    Distortion0,
+    Distortion1,
+    Distortion2,
+    Distortion3,
+    Distortion4
+};
+
+struct TumReprojectionError
+{
+    TumReprojectionError(double observed_x, double observed_y, 
+        double focal_x, double focal_y, double center_x, double center_y)
+    : observed_x_(observed_x), observed_y_(observed_y),
+        focal_x_(focal_x), focal_y_(focal_y),
+        center_x_(center_x), center_y_(center_y) {}
+
+    template <typename T>
+    bool operator()(const T* const camera, const T* const point, T* residuals) const
+    {
+        T p[3];
+        T quat[4] = {
+            -camera[TumCamera::QuaternionX],
+            -camera[TumCamera::QuaternionY],
+            -camera[TumCamera::QuaternionZ],
+            camera[TumCamera::QuaternionW]};
+
+        ceres::QuaternionRotatePoint(quat, point, p);
+        p[0] -= camera[TumCamera::TranslateX];
+        p[1] -= camera[TumCamera::TranslateY];
+        p[2] -= camera[TumCamera::TranslateZ];
+
+        T xp = static_cast<T>(focal_x_)*point[Point::x]/point[Point::z] + static_cast<T>(center_x_);
+        T yp = static_cast<T>(focal_y_)*point[Point::y]/point[Point::z] + static_cast<T>(center_y_);
+
+        residuals[0] = xp - observed_x_;
+        residuals[1] = yp - observed_y_;
+        return true;
+    }
+
+    static ceres::CostFunction* Create(const double observed_x, const double observed_y, const double camera[])
+    {
+        return (new ceres::AutoDiffCostFunction<TumReprojectionError,2,7,3>(
+            new TumReprojectionError(observed_x, observed_y, 
+                camera[TumCamera::FocalX], camera[TumCamera::FocalY],
+                camera[TumCamera::CenterX], camera[TumCamera::CenterY])
+        ));
+    }
+
+    private:
+        double observed_x_;
+        double observed_y_;
+        double focal_x_;
+        double focal_y_;
+        double center_x_;
+        double center_y_;
+        double distortion[4];
+};
+
+template <int NumCameras, int NumPoints, int NumObservations, int CameraDim>
+struct BundleAdjustmentProblem
+{
+    double cameras[NumCameras][CameraDim];
+    double points[NumPoints][3];
+    Observation observations[NumObservations];
+};
 }
